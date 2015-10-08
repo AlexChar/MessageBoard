2249b05d4e285e603070493a9933f7fc60ca1de1
AlexChar <alex.charizopoulos@gmail.com>
2015-10-09 00:17:18 +0300

    add angular

---
+++ MessageBoard/js/home-index.js
@@ -0,0 +1,21 @@
+﻿//home-index.js
+
+var app = angular.module("myapp", []);
+ 
+app.controller("homeIndexController", ["$scope", "$http", function ($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("http://localhost/MessageBoard/api/v1/topics?includeReplies=true")
+        .then(function (result) {
+            //success
+            angular.copy(result.data, $scope.data);
+        }, function () {
+            //error
+            console.error("could not load topics");
+        })
+        .then(function () {
+            $scope.isBusy = false;
+        });
+
+}]);
